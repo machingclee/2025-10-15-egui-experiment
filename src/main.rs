3f4e3afa79016c464b7d63c62afe98dcde9735a4
machingclee <machingclee@gmail.com>
2025-10-15 03:29:24 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+// When compiling natively:
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1280.0, 720.0])
+            .with_min_inner_size([800.0, 400.0])
+            .with_icon(
+                // NOTE: Adding an icon is optional
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                    .expect("Failed to load icon"),
+            ),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Shell Script Managers",
+        native_options,
+        Box::new(|cc| Ok(Box::new(eframe_template::App::new(cc)))),
+    )
+}
